47ec164cacba89a2c752f61766a7ac20ff537c8e
Bo Marchman <bo.marchman@gmail.com>
2014-08-23 03:03:24 -0400

    campsite residency dates are filled

--- trip/admin.py
+++ trip/admin.py
@@ -1,5 +1,6 @@
 
 import collections
+from datetime import datetime, timedelta
 
 from django.contrib import admin
 from django.db.models import Q
@@ -10,18 +11,12 @@ from trip.models import Campsite, TripType, TripTemplate, ScheduledTrip, Section
 class CampsiteAdmin(admin.ModelAdmin):
 
     list_display = ['name', 'dates']
-
-    def dates(self, campsite): 
-        """ Display the number of people staying at a campsite 
-        (supposing all trips are maxed out) for all dates.
-        """
-        
-        # TODO: set table header of dates, instead of literal 'dates' string
+    
+    def get_camping_dates(self, trips_year):
 
         # -----------------
         # TODO: move this somewhere like the section object or manager
         # TODO: must support archival views - doesn't this? vv
-        trips_year = campsite.trips_year
 
         sections = Section.objects.filter(trips_year=trips_year)
 
@@ -31,6 +26,17 @@ class CampsiteAdmin(admin.ModelAdmin):
         
         camping_dates = sorted(list(nights_camping))
         # -------------------------
+        return camping_dates
+        
+
+    def dates(self, campsite): 
+        """ Display the number of people staying at a campsite 
+        (supposing all trips are maxed out) for all dates.
+        """
+        trips_year = campsite.trips_year
+        camping_dates = self.get_camping_dates(trips_year)
+        
+        # TODO: set table header of dates, instead of literal 'dates' string
 
         # all trips which stay at campsite
         resident_trips = (ScheduledTrip.objects
@@ -48,7 +54,61 @@ class CampsiteAdmin(admin.ModelAdmin):
         
         return ' | '.join(map(lambda d: str(num_people_per_date.get(d, '-')), camping_dates))
 
-        # return '|'.join(map(str, camping_dates))
+    # return '|'.join(map(str, camping_dates))
+
+    def get_list_display(self, request):
+        
+        """ Add all dates camping for the current section. 
+
+        Creates a table of campsite occupancies. 
+        """
+
+        # WARNING
+        # TODO: get this from request, not thin air
+        trips_year = TripsYear.objects.current()
+        camping_dates = self.get_camping_dates(trips_year)
+
+        return self.list_display + list(map(str, camping_dates))
+
+    def __getattr__(self, attr):
+        """ Allow list_displayed camping dates to be accessed
+
+        These dates return ____
+
+        TODO: is there a more efficient way to do this? We're currently 
+        making m*n queries, one for each entry in the table. Can we cache anything?
+
+        TODO: write test for this.
+        """
+        try:
+            date = datetime.strptime(attr, '%Y-%m-%d').date()
+
+            def date_capacity_getter(campsite):
+                
+                trips_year = campsite.trips_year
+                
+                # timedelta do the same thing as at_campsite_1/2 properties.
+                # We want all trips which are staying at campsite on date.
+                resident_trips = ScheduledTrip.objects.filter(trips_year=trips_year).filter(
+                    Q(template__campsite_1=campsite, 
+                      section__leaders_arrive=date-timedelta(days=2)) | 
+                    Q(template__campsite_2=campsite,
+                      section__leaders_arrive=date-timedelta(days=3)))
+
+                num_residents = 0
+                for trip in resident_trips:
+                    if date == trip.section.at_campsite_1 and campsite == trip.template.campsite_1:
+                        num_residents += trip.template.max_num_people
+                    if date == trip.section.at_campsite_2 and campsite == trip.template.campsite_2:
+                        num_residents += trip.template.max_num_people
+                return num_residents
+
+            date_capacity_getter.short_description = date.strftime('%m-%d')
+            
+            return date_capacity_getter
+            
+        except ValueError:
+            return super(CampsiteAdmin, self).__getattr__(self, attr)
 
 
 class TripTypeAdmin(admin.ModelAdmin):
