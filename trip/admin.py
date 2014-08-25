8300ca329ff8492999c017be48cf15e1dbc00672
Bo Marchman <bo.marchman@gmail.com>
2014-08-25 00:27:27 -0400

    linted

--- trip/admin.py
+++ trip/admin.py
@@ -8,10 +8,11 @@ from django.db.models import Q
 from db.models import TripsYear
 from trip.models import Campsite, TripType, TripTemplate, ScheduledTrip, Section
 
+
 class CampsiteAdmin(admin.ModelAdmin):
 
     list_display = ['name', 'dates']
-    
+
     def get_camping_dates(self, trips_year):
 
         # -----------------
@@ -20,28 +21,28 @@ class CampsiteAdmin(admin.ModelAdmin):
 
         sections = Section.objects.filter(trips_year=trips_year)
 
-        nights_camping = set() 
+        nights_camping = set()
         for section in sections:
             nights_camping.update(section.nights_camping)
-        
+
         camping_dates = sorted(list(nights_camping))
         # -------------------------
         return camping_dates
-        
 
-    def dates(self, campsite): 
-        """ Display the number of people staying at a campsite 
+    def dates(self, campsite):
+        """ Display the number of people staying at a campsite
         (supposing all trips are maxed out) for all dates.
         """
         trips_year = campsite.trips_year
         camping_dates = self.get_camping_dates(trips_year)
-        
+
         # TODO: set table header of dates, instead of literal 'dates' string
 
         # all trips which stay at campsite
         resident_trips = (ScheduledTrip.objects
-            .filter(trips_year=trips_year)
-            .filter(Q(template__campsite_1=campsite) | Q(template__campsite_2=campsite)))
+                          .filter(trips_year=trips_year)
+                          .filter(Q(template__campsite_1=campsite) |
+                                  Q(template__campsite_2=campsite)))
 
         # mapping of dates -> number of people staying at campsite on that date
         num_people_per_date = collections.defaultdict(int)
@@ -51,16 +52,16 @@ class CampsiteAdmin(admin.ModelAdmin):
                 num_people_per_date[trip.section.at_campsite_1] += trip.template.max_num_people
             if trip.template.campsite_2 == campsite:
                 num_people_per_date[trip.section.at_campsite_2] += trip.template.max_num_people
-        
+
         return ' | '.join(map(lambda d: str(num_people_per_date.get(d, '-')), camping_dates))
 
     # return '|'.join(map(str, camping_dates))
 
     def get_list_display(self, request):
-        
-        """ Add all dates camping for the current section. 
 
-        Creates a table of campsite occupancies. 
+        """ Add all dates camping for the current section.
+
+        Creates a table of campsite occupancies.
         """
 
         # WARNING
@@ -75,8 +76,9 @@ class CampsiteAdmin(admin.ModelAdmin):
 
         These dates return ____
 
-        TODO: is there a more efficient way to do this? We're currently 
-        making m*n queries, one for each entry in the table. Can we cache anything?
+        TODO: is there a more efficient way to do this? We're currently
+        making m*n queries, one for each entry in the table. Can we cache
+        anything?
 
         TODO: write test for this.
         """
@@ -84,29 +86,36 @@ class CampsiteAdmin(admin.ModelAdmin):
             date = datetime.strptime(attr, '%Y-%m-%d').date()
 
             def date_capacity_getter(campsite):
-                
+
                 trips_year = campsite.trips_year
-                
+
                 # timedelta do the same thing as at_campsite_1/2 properties.
                 # We want all trips which are staying at campsite on date.
-                resident_trips = ScheduledTrip.objects.filter(trips_year=trips_year).filter(
-                    Q(template__campsite_1=campsite, 
-                      section__leaders_arrive=date-timedelta(days=2)) | 
-                    Q(template__campsite_2=campsite,
-                      section__leaders_arrive=date-timedelta(days=3)))
+                resident_trips = (
+                    ScheduledTrip.objects
+                    .filter(trips_year=trips_year)
+                    .filter(Q(template__campsite_1=campsite,
+                            section__leaders_arrive=date-timedelta(days=2)) |
+                            Q(template__campsite_2=campsite,
+                            section__leaders_arrive=date-timedelta(days=3))))
 
                 num_residents = 0
                 for trip in resident_trips:
-                    if date == trip.section.at_campsite_1 and campsite == trip.template.campsite_1:
+
+                    if (date == trip.section.at_campsite_1 and
+                            campsite == trip.template.campsite_1):
                         num_residents += trip.template.max_num_people
-                    if date == trip.section.at_campsite_2 and campsite == trip.template.campsite_2:
+
+                    if (date == trip.section.at_campsite_2 and
+                            campsite == trip.template.campsite_2):
                         num_residents += trip.template.max_num_people
+
                 return num_residents
 
             date_capacity_getter.short_description = date.strftime('%m-%d')
-            
+
             return date_capacity_getter
-            
+
         except ValueError:
             return super(CampsiteAdmin, self).__getattr__(self, attr)
 
@@ -124,7 +133,7 @@ class TripTypeAdmin(admin.ModelAdmin):
 
 
 class TripTemplateAdmin(admin.ModelAdmin):
-    
+
     list_display = ['name', 'description', 'trip_type']
 
 
