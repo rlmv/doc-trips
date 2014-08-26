f023945523ce8ef3a86dc15790c03b223ef3e9c9
Bo Marchman <bo.marchman@gmail.com>
2014-08-26 09:33:06 -0400

    rearrange test structure

--- db/test.py
+++ db/test.py
@@ -2,14 +2,22 @@
 from django.test import TestCase
 from django.db import models
 from django.test.utils import override_settings
+from django.conf.urls import patterns, url
 from model_mommy import mommy
 
 from db.models import DatabaseModel, TripsYear
 from test.fixtures import init_trips_year
 
+
 class ExampleDatabaseModel(DatabaseModel):
     """ Mock class inheriting DatabaseModel """
     some_field = models.CharField(max_length=255)
+
+
+""" Test urls for test_get_absolute_url """
+urlpatterns = patterns('', 
+    url(r'^(?P<trips_year>[0-9]+)/(?P<pk>[0-9]+)$', lambda r: None, name='test_url')
+)
 
 
 class DatabaseModelTestCase(TestCase):
@@ -30,13 +38,14 @@ class DatabaseModelTestCase(TestCase):
         self.assertNotEqual(e2.trips_year, self.trips_year, 
                             'saving should not overide explicitly specified trip_year')
 
-    @override_settings(ROOT_URLCONF='db.test.test_urls')
+    @override_settings(ROOT_URLCONF='db.test.test')
     def test_get_absolute_url(self):
         
         e = ExampleDatabaseModel(some_field='ocho')
         e.save()
         e.absolute_url_pattern = 'test_url'
         e.get_absolute_url()
+
 
 class CalendarTestCase(TestCase):
 
