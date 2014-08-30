f9ce032270ef9e85c3928d70a34943ee468e2902
Bo Marchman <bo.marchman@gmail.com>
2014-08-30 17:21:57 -0400

    delete_link tag comments

--- doc-trips/db/templatetags/links.py
+++ doc-trips/db/templatetags/links.py
@@ -7,13 +7,12 @@ register = template.Library()
 @register.filter
 def edit_link(db_object):
     """ Insert the html link to edit db_object """
-
     html = '<a href="{}">edit</a>'.format(db_object.get_update_url())
     return mark_safe(html)
 
 @register.filter
 def delete_link(db_object):
-    
+    """ Insert html link to delete db_object """
     html = '<a href="{}">delete</a>'.format(db_object.get_delete_url())
     return mark_safe(html)
     
