7a94c38c2d709f3a7b7c2112ea0ab8b8b512a715
Bo Marchman <bo.marchman@gmail.com>
2014-08-30 17:21:18 -0400

    add delete_link template filter

--- doc-trips/db/templatetags/links.py
+++ doc-trips/db/templatetags/links.py
@@ -5,9 +5,16 @@ from django.utils.safestring import mark_safe
 register = template.Library()
 
 @register.filter
-def edit_link(object):
-    """ Return an html link to the update view for object 
+def edit_link(db_object):
+    """ Insert the html link to edit db_object """
 
-    TODO: move html into the template itself? """
-    html = '<a href="{}">edit</a>'.format(object.get_update_url())
+    html = '<a href="{}">edit</a>'.format(db_object.get_update_url())
     return mark_safe(html)
+
+@register.filter
+def delete_link(db_object):
+    
+    html = '<a href="{}">delete</a>'.format(db_object.get_delete_url())
+    return mark_safe(html)
+    
+    
