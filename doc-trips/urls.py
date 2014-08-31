ab80c95de622fbe10776cd16ec8323925dbafeb4
Bo Marchman <bo.marchman@gmail.com>
2014-08-31 16:34:07 -0400

    rm unneccessary index view

--- doc-trips/urls.py
+++ doc-trips/urls.py
@@ -5,10 +5,8 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'views.index', name='index'),
     url(r'^user/', include('user.urls', namespace='user')),
     url(r'^db/', include('db.urls', namespace='db')),
     url(r'^leader/', include('leader.urls', namespace='leader')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^grappelli/', include('grappelli.urls')),
 )
