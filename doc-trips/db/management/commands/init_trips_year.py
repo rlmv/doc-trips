5e9f5be9058844e888b2d4f7afc8df8c6453276a
Bo Marchman <bo.marchman@gmail.com>
2014-09-18 14:55:41 -0400

    Add init_trips_year management command

---
+++ doc-trips/db/management/commands/init_trips_year.py
@@ -0,0 +1,28 @@
+
+
+from django.core.management.base import BaseCommand
+
+from db.models import TripsYear
+
+class Command(BaseCommand):
+
+    args = '<trips_year>'
+    help = 'Initialize the database with a current trips_year'
+
+    def handle(self, *args, **options):
+        
+        try:
+            trips_year = TripsYear.objects.current()
+            msg = ('a current TripsYear (%d) already exists in the database')
+            self.stderr.write(msg % trips_year.year)
+
+        except TripsYear.DoesNotExist:
+            if len(args) == 1:
+                TripsYear.objects.create(trips_year=args[1], is_current=True)
+
+            
+
+            
+            
+            
+        
