5bb23d58285316e91b8c0bf2612acb616e6a2bc5
Bo Marchman <bo.marchman@gmail.com>
2015-01-28 17:37:08 -0500

    Fix croo migrations

--- doc-trips/croos/migrations/0001_initial.py
+++ doc-trips/croos/migrations/0001_initial.py
@@ -2,8 +2,8 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import django.db.models.deletion
 from django.conf import settings
+import django.db.models.deletion
 
 
 class Migration(migrations.Migration):
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Croo',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
                 ('name', models.CharField(max_length=255)),
                 ('trips_year', models.ForeignKey(to='db.TripsYear', editable=False)),
             ],
@@ -29,11 +29,11 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='CrooApplication',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
                 ('safety_dork_qualified', models.BooleanField(default=False)),
                 ('safety_dork', models.BooleanField(default=False)),
                 ('applicant', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
-                ('assigned_croo', models.ForeignKey(to='croos.Croo', null=True, related_name='croolings', blank=True, on_delete=django.db.models.deletion.SET_NULL)),
+                ('assigned_croo', models.ForeignKey(related_name='croolings', to='croos.Croo', blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL)),
                 ('potential_croos', models.ManyToManyField(related_name='potential_croolings', to='croos.Croo', blank=True)),
                 ('trips_year', models.ForeignKey(to='db.TripsYear', editable=False)),
             ],
@@ -45,9 +45,9 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='CrooApplicationAnswer',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
                 ('answer', models.TextField()),
-                ('application', models.ForeignKey(to='croos.CrooApplication', editable=False, related_name='answers')),
+                ('application', models.ForeignKey(related_name='answers', to='croos.CrooApplication', editable=False)),
             ],
             options={
                 'abstract': False,
@@ -57,7 +57,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='CrooApplicationGrade',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
                 ('grade', models.IntegerField()),
                 ('comments', models.TextField()),
                 ('application', models.ForeignKey(related_name='grades', to='croos.CrooApplication')),
@@ -72,7 +72,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='CrooApplicationQuestion',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
                 ('question', models.TextField()),
                 ('ordering', models.IntegerField()),
                 ('trips_year', models.ForeignKey(to='db.TripsYear', editable=False)),
