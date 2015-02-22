8ad6ab164c06bc330097dcba0eba4f8c9ac2cbeb
Bo Marchman <bo.marchman@gmail.com>
2015-02-22 17:41:44 -0500

    Change app icons

--- doc-trips/db/templatetags/icons.py
+++ doc-trips/db/templatetags/icons.py
@@ -5,7 +5,7 @@ from django.utils.safestring import mark_safe
 register = template.Library()
 
 @register.filter
-def checkmark_if_true(condition):
+def ok_if_true(condition):
     """ If condition is true, insert an 'ok' tick mark. """
 
     if condition:
@@ -13,4 +13,14 @@ def checkmark_if_true(condition):
         return mark_safe(html)
 
     return ''
+
+@register.filter
+def checkmark_if_true(condition):
+    """ If condition is true, show a checked box; otherwise, show empty box """
+    
+    if condition:
+        html = '<span class="glyphicon glyphicon-check" aria-hidden="true"></span>' 
+    else:
+        html = '<span class="glyphicon glyphicon-unchecked" aria-hidden="true"></span>'
+    return mark_safe(html)
     
