c8d1f45581f47842eb3a40067802940a58e72669
Bo Marchman <bo.marchman@gmail.com>
2015-02-26 15:51:58 -0500

    Don't have request in scope

--- doc/dartdm/lookup.py
+++ doc/dartdm/lookup.py
@@ -32,7 +32,7 @@ def lookup_email(netid):
     # netid not found: {}
     if not r.json(): 
         msg = 'Email lookup failed: NetId %s not found' % netid
-        logger.error(msg, extra={'request': request})
+        logger.error(msg)
         raise EmailLookupException(msg)
     
     # mismatch, somehow...
