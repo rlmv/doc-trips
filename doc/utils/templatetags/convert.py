19eba3c6e5bb8a06a8d1589f93e289f53d0cbae4
Bo Marchman <bo.marchman@gmail.com>
2015-02-27 17:07:22 -0500

    Docx filefield formatting

--- doc/utils/templatetags/convert.py
+++ doc/utils/templatetags/convert.py
@@ -9,4 +9,15 @@ register = template.Library()
 @register.simple_tag
 def display_docx_filefield_as_html(filefield):
 
-    return convert_docx_filefield_to_html(filefield)
+    t = template.Template("""
+    <div class="alert alert-info" role="alert">
+    Can't see the application, or the formatting is wonky? 
+    <a href="{{ filefield.url }}">Download the application here </a>
+    </div>
+    <div class="well">
+    {{ html }}
+    </div>
+    """)
+    c = template.Context({'filefield': filefield, 
+                          'html': convert_docx_filefield_to_html(filefield) })
+    return t.render(c)
