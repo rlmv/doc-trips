cd3b4c5627e7b5b0caf33836d9ec35935ff989cd
Bo Marchman <bo.marchman@gmail.com>
2015-03-11 13:59:05 -0400

    Add completed registration view

--- doc/trippees/urls.py
+++ doc/trippees/urls.py
@@ -1,12 +1,13 @@
 
 from django.conf.urls import patterns, url, include
 
-from doc.trippees.views import Register, EditRegistration
+from doc.trippees.views import Register, EditRegistration, ViewRegistration
 
 urlpatterns = patterns(
     '',
     url(r'^register/$', Register.as_view(), name='register'),
     url(r'^register/edit/$', EditRegistration.as_view(), name='edit_registration'),
+    url(r'^register/view/$', ViewRegistration.as_view(), name='view_registration'),
 )
 
 
--- doc/trippees/views.py
+++ doc/trippees/views.py
@@ -1,6 +1,6 @@
 
-
-from vanilla import CreateView
+from django.core.urlresolvers import reverse_lazy
+from vanilla import CreateView, UpdateView, DetailView
 from braces.views import LoginRequiredMixin
 
 from doc.trippees.models import TrippeeRegistration
@@ -9,11 +9,11 @@ from doc.trippees.forms import RegistrationForm
 class Register(LoginRequiredMixin, CreateView):
     """ 
     Register for trips 
-    
     """
     model = TrippeeRegistration
     template_name = 'trippees/register.html'
     form_class = RegistrationForm
+    success_url = reverse_lazy('trippees:view_registration')
 
     def form_valid(self, form, **kwargs):
         """ 
@@ -26,13 +26,14 @@ class Register(LoginRequiredMixin, CreateView):
         return super(Register, self).form_valid(form, **kwargs)
 
 
-class EditRegistration(LoginRequiredMixin, CreateView):
+class EditRegistration(LoginRequiredMixin, UpdateView):
     """
     Edit a trippee registration.
     """
     model = TrippeeRegistration
     template_name = 'trippees/register.html'
     form_class = RegistrationForm
+    success_url = reverse_lazy('trippees:view_registration')
 
     def get_object(self):
         return self.model.objects.filter(
@@ -41,4 +42,22 @@ class EditRegistration(LoginRequiredMixin, CreateView):
         )
            
  
+class ViewRegistration(LoginRequiredMixin, DetailView):
+    """
+    View a complete registration 
+    """
+    model = TrippeeRegistration
+    template_name = 'trippees/completed_registration.html'
+    fields = ['name']
+    
+    def get_object(self):
+        return self.model.objects.filter(
+            trips_year=TripsYear.objects.current(),
+            user=self.request.user
+        )
+         
+    
+    
+ 
+
     
