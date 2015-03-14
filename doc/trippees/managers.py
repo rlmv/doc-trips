32e3de7c9607bba72b3c4ee59ee24526e96cf1e5
Bo Marchman <bo.marchman@gmail.com>
2015-03-13 23:30:20 -0400

    Refactor incoming upload

--- doc/trippees/managers.py
+++ doc/trippees/managers.py
@@ -10,43 +10,48 @@ class CollegeInfoManager(models.Manager):
 
     def create_from_csv_file(self, file, trips_year):
         """
+        Import incoming students from a CSV file. 
 
+        If a student already exists in the database for this year, 
+        ignore. We compare entries via netid. 
+        
         trips_year is a string
-
         Returns a tuple (created_students, already_existing_students).
+
+        TODO: parse/input incoming_status. How should this work?
         """
 
         trips_year = TripsYear.objects.get(pk=trips_year)
  
-        info = []
         reader = csv.DictReader(file)
-        for row in reader:
+            
+        def parse_to_object(row):
+            """ Parse a CSV row and return an incoming student object """
+            return self.model(
+                trips_year=trips_year,
+                netid=row['Id'],
+                name=row['Formatted Fml Name'],
+                class_year=row['Class Year'],
+                gender=row['Gender'],
+                ethnic_code=row['Fine Ethnic Code'],
+                email=row['EMail'],
+                dartmouth_email=row['Blitz'],
+            )
 
-            kwargs = {
-                'netid': row['Id'],
-                'name': row['Formatted Fml Name'],
-                'class_year': row['Class Year'],
-                'gender': row['Gender'],
-                'ethnic_code': row['Fine Ethnic Code'],
-                'email': row['EMail'],
-                'dartmouth_email': row['Blitz'],
-            }
-            kwargs['trips_year'] = trips_year
-
-            info.append(self.model(**kwargs))
+        incoming = list(map(parse_to_object, reader))
 
         def get_netids(incoming_students):
             return set(map(lambda x: x.netid, incoming_students))
 
-        netids = get_netids(info)
+        incoming_netids = get_netids(incoming)
         existing = self.model.objects.filter(trips_year=trips_year)
         existing_netids = get_netids(existing)
 
-        netids_to_create = netids - existing_netids
-        to_create = filter(lambda x: x.netid in netids_to_create, info)
+        netids_to_create = incoming_netids - existing_netids
+        to_create = filter(lambda x: x.netid in netids_to_create, incoming)
         self.model.objects.bulk_create(to_create)
 
-        ignored_netids = existing_netids & netids
+        ignored_netids = existing_netids & incoming_netids
         return (list(netids_to_create), list(ignored_netids))
         
        
