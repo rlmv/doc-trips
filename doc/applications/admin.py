d94a11049cee2b30b84218176a00dca40a4d816a
Bo Marchman <bo.marchman@gmail.com>
2015-03-17 19:44:54 -0400

    Add skips to admin

--- doc/applications/admin.py
+++ doc/applications/admin.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 
-from doc.applications.models import GeneralApplication, CrooSupplement, LeaderSupplement, CrooApplicationGrade, LeaderApplicationGrade, QualificationTag
+from doc.applications.models import GeneralApplication, CrooSupplement, LeaderSupplement, CrooApplicationGrade, LeaderApplicationGrade, QualificationTag, SkippedCrooGrade, SkippedLeaderGrade
 
 admin.site.register(GeneralApplication)
 admin.site.register(CrooSupplement)
@@ -9,4 +9,6 @@ admin.site.register(LeaderSupplement)
 admin.site.register(CrooApplicationGrade)
 admin.site.register(LeaderApplicationGrade)
 admin.site.register(QualificationTag)
+admin.site.register(SkippedCrooGrade)
+admin.site.register(SkippedLeaderGrade)
 
