af3ca1d5cd83a2465cb3ccdc18cfc49a5ebd1657
Bo Marchman <bo.marchman@gmail.com>
2015-03-19 11:06:16 -0400

    Pass jQuery to lookahead initialization

--- doc/dartdm/static/dartdm/lookup.js
+++ doc/dartdm/static/dartdm/lookup.js
@@ -1,23 +1,25 @@
 
-var netIds = new Bloodhound({
-    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
-    queryTokenizer: Bloodhound.tokenizers.whitespace,
-    remote: "/dartdm/lookup/?term=%QUERY"
-});
- 
-netIds.initialize();
- 
-$(".dartdmLookup").typeahead(null, {
-    name: 'net-id',
-    displayKey: 'value',
-    source: netIds.ttAdapter()
-});
+(function($) {
+    var netIds = new Bloodhound({
+	datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+	queryTokenizer: Bloodhound.tokenizers.whitespace,
+	remote: "/dartdm/lookup/?term=%QUERY"
+    });
+    
+    netIds.initialize();
+    
+    $(".dartdmLookup").typeahead(null, {
+	name: 'net-id',
+	displayKey: 'value',
+	source: netIds.ttAdapter()
+    });
 
-$( ".dartdmLookup" ).on("typeahead:selected", function (event, object, name) {
-    // autofill hidden netid and name fields
-    var parent = $( this ).parent( ".twitter-typeahead" );
-    var netIdField = $( parent ).next( ".netIdField" ).val(object.id);
-    $(netIdField).next( ".nameWithAffilField" ).val(object.value);
+    $( ".dartdmLookup" ).on("typeahead:selected", function (event, object, name) {
+	// autofill hidden netid and name fields
+	var parent = $( this ).parent( ".twitter-typeahead" );
+	var netIdField = $( parent ).next( ".netIdField" ).val(object.id);
+	$(netIdField).next( ".nameWithAffilField" ).val(object.value);
 
-});
+    });
+})(jQuery);
 
