cbe53753549121f637a6dc8aefe5d856d2a4f25a
Bo Marchman <bo.marchman@gmail.com>
2015-03-21 14:19:04 -0400

    Change 'applications' -> 'volunteers' in nav

--- doc/urls.py
+++ doc/urls.py
@@ -9,15 +9,20 @@ admin.autodiscover()
 initialize_groups_and_permissions()
 handler403 = 'doc.views.permission_denied'
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(r'^$', HomePage.as_view(), name='home'),
     url(r'^users/', include('doc.users.urls', namespace='users')),
-    url(r'^dartdm/', include('doc.dartdm.urls', namespace='dartdm')),                   
+    url(r'^dartdm/', include('doc.dartdm.urls', namespace='dartdm')),      
     url(r'^permissions/', include('doc.permissions.urls', namespace='permissions')),
     url(r'^timetable/', include('doc.timetable.urls', namespace='timetable')),
     url(r'^db/', include('doc.db.urls', namespace='db')),
+
+    url(r'^volunteers/', include('doc.applications.urls', namespace='applications')),
+    # TODO: remove the 'applications' urls in favor of 'volunteers'
     url(r'^applications/', include('doc.applications.urls', namespace='applications')),
-    url(r'^croos/', include('doc.croos.urls', namespace='croos')),                   
+
+    url(r'^croos/', include('doc.croos.urls', namespace='croos')),
     url(r'^incoming/', include('doc.trippees.urls', namespace='trippees')),
     url(r'^admin/', include(admin.site.urls)),
 )
