94695e9244749099126f846912b229d01c29c8c4
Bo Marchman <bo.marchman@gmail.com>
2015-03-21 13:42:59 -0400

    Fix test

--- doc/transport/tests.py
+++ doc/transport/tests.py
@@ -23,11 +23,9 @@ class ManagersTestCase(TripsYearTestCase):
     def test_external(self):
         
         trips_year = self.init_current_trips_year()
-        
         external_stop = mommy.make(Stop, trips_year=trips_year, category='EXTERNAL')
         internal_stop = mommy.make(Stop, trips_year=trips_year, category='INTERNAL')
-
-        self.assertEqual([external_stop], Stop.objects.external(trips_year))
+        self.assertEqual([external_stop], list(Stop.objects.external(trips_year)))
 
 
 class TestViews(WebTestCase):
