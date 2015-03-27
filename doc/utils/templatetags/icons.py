e1162a7faf4c4942a9318443d778dbb1fde42954
Bo Marchman <bo.marchman@gmail.com>
2015-03-27 10:11:21 -0400

    Use font-awesome

--- doc/utils/templatetags/icons.py
+++ doc/utils/templatetags/icons.py
@@ -4,23 +4,23 @@ from django.utils.safestring import mark_safe
 
 register = template.Library()
 
+
 @register.filter
 def ok_if_true(condition):
     """ If condition is true, insert an 'ok' tick mark. """
 
     if condition:
-        html = '<div class="text-center"><span class="glyphicon glyphicon-ok"/></div>'
+        html = '<i class="fa fa-check"></i>'
         return mark_safe(html)
-
     return ''
+
 
 @register.filter
 def checkmark_if_true(condition):
     """ If condition is true, show a checked box; otherwise, show empty box """
-    
+      
     if condition:
-        html = '<span class="glyphicon glyphicon-check" aria-hidden="true"></span>' 
+        html = '<i class="fa fa-check-square-o"></i>'
     else:
-        html = '<span class="glyphicon glyphicon-unchecked" aria-hidden="true"></span>'
+        html = '<i class="fa fa-square-o"></i>'
     return mark_safe(html)
-    
