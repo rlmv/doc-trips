cc868b571b8b39c51b65f5f68778a91eac34c825
Bo Marchman <bo.marchman@gmail.com>
2015-04-02 17:59:45 -0400

    Add crispify helper

--- doc/applications/views/assign.py
+++ doc/applications/views/assign.py
@@ -1,7 +1,6 @@
 from vanilla import UpdateView
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit
 
+from doc.utils.forms import crispify
 from doc.permissions.views import ApplicationEditPermissionRequired
 from doc.db.views import TripsYearMixin
 from doc.applications.models import GeneralApplication
@@ -21,7 +20,4 @@ class AssignToTrip(ApplicationEditPermissionRequired, TripsYearMixin,
 
     def get_form(self, **kwargs):
         form = super(AssignToTrip, self).get_form(**kwargs)
-        form.helper = FormHelper(form)
-        form.helper.add_input(Submit('submit', 'Update Assignment'))
-        return form
-   
+        return crispify(form, submit_text='Update Assignment')
---
+++ doc/utils/forms.py
@@ -0,0 +1,15 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+
+
+def crispify(form, submit_text=None):
+    """
+    Add a crispy form helper and submit button to form.
+
+    """
+    if submit_text is None:
+        submit_text = 'Submit'
+    form.helper = FormHelper(form)
+    form.helper.add_input(Submit('submit', submit_text))
+    return form
+    
