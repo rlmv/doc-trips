eab738ab7032c31e6c2cb2eb97cb508be7558a6f
Bo Marchman <bo.marchman@gmail.com>
2015-04-10 18:59:38 -0400

    Generic report view

--- doc/reports/tests.py
+++ doc/reports/tests.py
@@ -1,3 +1,37 @@
-from django.test import TestCase
+import io
+import csv
+import tempfile
 
-# Create your tests here.
+from django.core.urlresolvers import reverse
+from model_mommy import mommy
+
+from doc.test.fixtures import WebTestCase
+from doc.applications.tests import ApplicationTestMixin
+from doc.applications.models import GeneralApplication
+
+
+class ReportViewsTestCase(WebTestCase, ApplicationTestMixin):
+
+    def test_volunteer_csv(self):
+        trips_year = self.init_current_trips_year()
+        application = self.make_application(trips_year=trips_year)
+        non_applicant = self.make_application(trips_year=trips_year)
+        non_applicant.croo_supplement.document = ''
+        non_applicant.croo_supplement.save()
+        non_applicant.leader_supplement.document = ''
+        non_applicant.leader_supplement.save()
+        res = self.app.get(reverse('db:reports:application_csv',
+                                   kwargs={'trips_year': trips_year}),
+                           user=self.mock_director())
+        self.assertTrue(res['Content-Disposition'].startswith('attachment; filename="'))
+        # convolutedly save file content
+        f = tempfile.NamedTemporaryFile()
+        f.write(res.content)
+        f = open(f.name) # open in non-binary mode
+        reader = csv.reader(f)
+        row = next(reader)
+        self.assertEqual(row[0], application.applicant.name)
+        self.assertEqual(row[1], application.applicant.netid)
+        with self.assertRaises(StopIteration):
+            next(reader)
+        
--- doc/reports/views.py
+++ doc/reports/views.py
@@ -2,6 +2,7 @@ import csv
 
 from braces.views import AllVerbsMixin
 from vanilla import View
+from django.core.exceptions import ImproperlyConfigured
 from django.http import HttpResponse
 
 from doc.db.views import TripsYearMixin
@@ -11,20 +12,38 @@ from doc.permissions.views import DatabaseReadPermissionRequired
 
 class GenericReportView(DatabaseReadPermissionRequired,
                         TripsYearMixin, AllVerbsMixin, View):
-    pass
+    
+    file_prefix = None
+    
+    def get_filename(self):
+        return "{}-{}.csv".format(
+            self.file_prefix, self.kwargs['trips_year']
+        )
+
+    def get_queryset(self):
+        raise ImproperlyConfigured('implement get_queryset()')
+
+    def get_row(self, obj):
+        raise ImproperlyConfigured('implement get_row()')
+
+    def all(self, request, *args, **kwargs):
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = (
+            'attachment; filename="{}"'.format(self.get_filename())
+        )
+        writer = csv.writer(response)
+        for obj in self.get_queryset():
+            writer.writerow(self.get_row(obj))
+        return response
 
 
 class VolunteerCSV(GenericReportView):
 
-    def all(self, request, *args, **kwargs):
-        trips_year = self.kwargs['trips_year']
-        response = HttpResponse(content_type='text/csv')
-        filename = 'TL-and-Croo-applicants-%s.csv' % trips_year
-        response['Content-Disposition'] = 'attachment; filename="%s"' % filename
-        writer = csv.writer(response)
-        qs = GeneralApplication.objects.leader_or_croo_applications(trips_year)
-        for application in qs:
-            user = application.applicant
-            writer.writerow([user.name, user.netid])
-        return response
+    file_prefix = 'TL-and-Croo-applicants'
 
+    def get_queryset(self):
+        return GeneralApplication.objects.leader_or_croo_applications(self.kwargs['trips_year'])
+
+    def get_row(self, application):
+        user = application.applicant
+        return [user.name, user.netid]
