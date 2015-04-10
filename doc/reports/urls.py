0ee5f1229aac6c2983f684de45f293ceb188b4e3
Bo Marchman <bo.marchman@gmail.com>
2015-04-10 19:26:16 -0400

    Show TL report

--- doc/reports/urls.py
+++ doc/reports/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 
-from doc.reports.views import VolunteerCSV
+from doc.reports.views import VolunteerCSV, TripLeaderApplicationsCSV
 
 urlpatterns = [
-    url(r'^export/$', VolunteerCSV.as_view(), name='application_csv'),
+    url(r'^applications/all/$', VolunteerCSV.as_view(), name='all_apps'),
+    url(r'^applications/trip-leaders/$', TripLeaderApplicationsCSV.as_view(), 
+        name='trip_leader_apps'),
 ]
