db43256ebc5418bec22803425795f07ed39f59f8
Bo Marchman <bo.marchman@gmail.com>
2015-05-12 22:01:43 -0400

    Fix migration

--- doc/applications/migrations/0063_auto_20150512_2128.py
+++ doc/applications/migrations/0063_auto_20150512_2128.py
@@ -11,28 +11,4 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-            model_name='leadersupplement',
-            name='community_building',
-            field=models.DateField(blank=True, null=True),
-            preserve_default=True,
-        ),
-        migrations.AddField(
-            model_name='leadersupplement',
-            name='first_aid',
-            field=models.DateField(verbose_name='First Aid/CPR', blank=True, null=True),
-            preserve_default=True,
-        ),
-        migrations.AddField(
-            model_name='leadersupplement',
-            name='risk_management',
-            field=models.DateField(blank=True, null=True),
-            preserve_default=True,
-        ),
-        migrations.AddField(
-            model_name='leadersupplement',
-            name='wilderness_skills',
-            field=models.DateField(blank=True, null=True),
-            preserve_default=True,
-        ),
     ]
