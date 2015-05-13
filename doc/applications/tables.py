614fdc3a50452bfc54096471d4c77f3e019a795a
Bo Marchman <bo.marchman@gmail.com>
2015-05-12 23:09:44 -0400

    Add training columns

--- doc/applications/tables.py
+++ doc/applications/tables.py
@@ -1,8 +1,28 @@
 import django_tables2 as tables
 from django.core.urlresolvers import reverse
+from django.utils.safestring import mark_safe
 
 from doc.db.templatetags.links import detail_link, make_link, edit_link
 from doc.utils.templatetags.icons import ok_if_true
+
+
+def tooltip_wrap(target, tooltip):
+    """ Wrap a string with a Bootstrap tooltip """
+    return mark_safe(
+        '<span data-toggle="tooltip" data-placement="top" title="{}"> {} </span>'.format(tooltip, target)
+    )
+
+
+class TrainingColumn(tables.Column):
+    """ Column with a tooltip """
+
+    def __init__(self, verbose_name, tooltip, *args, **kwargs):
+        self.tooltip = tooltip
+        verbose_name = tooltip_wrap(verbose_name, self.tooltip)
+        super(TrainingColumn, self).__init__(verbose_name, *args, **kwargs)
+
+    def render(self, value):
+        return tooltip_wrap(value.strftime("%m/%d").lstrip('0'), self.tooltip)
 
 
 class ApplicationTable(tables.Table):
@@ -20,10 +40,24 @@ class ApplicationTable(tables.Table):
         verbose_name='Croo score', order_by=('-avg_croo_grade')
     )
     leader_application = tables.Column(
+        verbose_name='Leader app',
         accessor='leader_application_complete', orderable=False
     )
     croo_application = tables.Column(
+        verbose_name='Croo app',
         accessor='croo_application_complete', orderable=False
+    )
+    community_building = TrainingColumn(
+        verbose_name='CB', tooltip="Community Building"
+    )
+    risk_management = TrainingColumn(
+        verbose_name='RM', tooltip="Risk Management"
+    )
+    wilderness_skills = TrainingColumn(
+        verbose_name='WS', tooltip="Wilderness Skills"
+    )
+    croo_training = TrainingColumn(
+        verbose_name='CT', tooltip="Croo Training"
     )
     view_link = tables.Column(
         verbose_name=' ', empty_values=(), orderable=False
