6efde9b029118dd3a3d7526b7a8f14f60bc3cc5c
Bo Marchman <bo.marchman@gmail.com>
2015-06-16 15:13:29 -0400

    Tidy up webauth backend

--- doc/webauth/backends.py
+++ doc/webauth/backends.py
@@ -1,5 +1,3 @@
-"""CAS authentication backend"""
-
 from urllib.parse import urljoin
 from xml.etree import ElementTree
 
@@ -15,9 +13,8 @@ def parse_cas_success(tree):
     """ 
     Callback function for parsing Dartmouth's CAS response.
     
-    Return the verified user. 
+    Returns the verified user.
     """
-
     def findtext(text):
         tag_prefix = "{http://www.yale.edu/tp/cas}"
         return tree[0].findtext(tag_prefix + text)
@@ -36,20 +33,15 @@ def verify(ticket, service):
 
     Returns user on success and None on failure.
     """
-
     params = {'ticket': ticket, 'service': service}
 
     # TODO: ensure that url uses https
     url = urljoin(settings.CAS_SERVER_URL, 'serviceValidate')
     r = requests.get(url, params=params)
 
-    # print(r.text)
     try:
         tree = ElementTree.fromstring(r.text)
-        
         if tree[0].tag.endswith('authenticationSuccess'):
-            #if settings.CAS_RESPONSE_CALLBACKS:
-            #    cas_response_callbacks(tree)
             return parse_cas_success(tree)
         else:
             return None
@@ -59,8 +51,9 @@ def verify(ticket, service):
 
 
 class WebAuthBackend(ModelBackend):
-    """ CAS authentication backend """
-
+    """ 
+    CAS authentication backend for Dartmouth Webauth
+    """
     def authenticate(self, ticket, service):
         """
         Verifies CAS ticket and gets or creates User object.
@@ -68,8 +61,9 @@ class WebAuthBackend(ModelBackend):
         return verify(ticket, service)
 
     def get_user(self, user_id):
-        """ Retrieve the user's entry in the User model if it exists """
-
+        """ 
+        Retrieve the user's entry in the User model if it exists 
+        """
         try:
             return UserModel.objects.get(pk=user_id)
         except UserModel.DoesNotExist:
