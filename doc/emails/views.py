a91eeb496d8e474783765a0d472371a9d863f6bb
Bo Marchman <bo.marchman@gmail.com>
2015-07-07 11:43:10 -0400

    Add trippee email lists

--- doc/emails/views.py
+++ doc/emails/views.py
@@ -39,6 +39,13 @@ def emails(qs):
     return list(map(lambda x: x['applicant__email'], values))
 
 
+def personal_emails(qs):
+    """ 
+    List of trippee emails from IncomingStudent qs
+    """
+    return list(map(lambda x: x.email, qs))
+
+
 class Applicants(BaseEmailList):
 
     headline = "Applicant Emails"
@@ -128,3 +135,26 @@ class IncomingStudents(BaseEmailList):
 
     def blitz(self, qs):
         return list(map(lambda x: x.blitz, qs))
+
+
+class Trippees(BaseEmailList):
+    
+    headline = "Trippees"
+
+    def get_email_lists(self):
+        trips_year = self.get_trips_year()
+        sections = Section.objects.filter(trips_year=trips_year)
+        trippees = IncomingStudent.objects.filter(trips_year=trips_year)
+        email_list = [
+            ("All Trippees (Incoming Students with a trip assignment)",
+             personal_emails(trippees.filter(trip_assignment__isnull=False)))
+        ]
+        for sxn in sections:
+            email_list.append((
+                "Section %s trippees" % sxn.name,
+                personal_emails(trippees.filter(trip_assignment__section=sxn))
+            ))
+        return email_list
+            
+        
+        
