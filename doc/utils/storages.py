e1277fa26207ed3f298b273a29c154b9732ae45a
Bo Marchman <bo.marchman@gmail.com>
2015-07-13 20:09:00 -0400

    Tidy storages

--- doc/utils/storages.py
+++ doc/utils/storages.py
@@ -2,15 +2,20 @@
 from django.conf import settings
 
 from storages.backends.s3boto import S3BotoStorage
-
-class S3FileStorage(S3BotoStorage):
-    """ File storage which prefixes the key with FILE_STORAGE_PREFIX  """
-    location = getattr(settings, 'FILE_STORAGE_PREFIX', None)
-
-
 from pipeline.storage import PipelineMixin
 from whitenoise.django import GzipManifestStaticFilesStorage
 
+
+class S3FileStorage(S3BotoStorage):
+    """
+    File storage which prefixes the key with FILE_STORAGE_PREFIX
+    """
+    location = getattr(settings, 'FILE_STORAGE_PREFIX', None)
+
+
 class GzipManifestPipelineStorage(PipelineMixin, GzipManifestStaticFilesStorage):
+    """
+    Use both Gzip and Pipeline for staticfiles
+    """
     pass
     
