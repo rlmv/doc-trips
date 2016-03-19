b7936d2037b4feac6a4677f7d30fdcf9d240d724
Bo Marchman <bo.marchman@gmail.com>
2016-03-19 11:53:53 -0400

    Tidy lookups

--- fyt/dartdm/lookup.py
+++ fyt/dartdm/lookup.py
@@ -7,9 +7,9 @@ logger = logging.getLogger(__name__)
 
 
 def dartdm_lookup(query_string):
-    """ 
+    """
     Search in the DartDm for a user.
-    
+
     TODO: catch requests library errors
     """
     payload = {'term': query_string}
@@ -21,26 +21,20 @@ def dartdm_lookup(query_string):
 class EmailLookupException(Exception):
     pass
 
+
 def lookup_email(netid):
-    """ 
+    """
     Lookup the email address of a user, given their NetId.
     """
-
     params = {'lookup': netid, 'fields': ['email', 'netid']}
     r = requests.get('http://dndprofiles.dartmouth.edu/profile', params=params)
 
-    # netid not found: {}
+    # netid not found
     if not r.json():
         msg = 'Email lookup failed: NetId %s not found' % netid
         logger.error(msg)
         raise EmailLookupException(msg)
 
-    # mismatch, somehow...
-    if r.json()['netid'] != netid:
-        raise EmailLookupException('Email lookup failed: NetId mismatch: %s != %s' % (r.json()['netid'],
-                                                                 netid))
+    assert r.json()['netid'] == netid
 
     return r.json()['email']
-
-
-
