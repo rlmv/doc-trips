90d4e20bf4dd737406bf558766d0ee4b85a5a764
Bo Marchman <bo.marchman@gmail.com>
2016-04-27 14:10:14 -0600

    Update user management commands

--- fyt/users/management/commands/rmsuperuser.py
+++ fyt/users/management/commands/rmsuperuser.py
@@ -9,22 +9,19 @@ class Command(BaseCommand):
     help = 'Revoke the superuser status of the given users'
 
     def add_arguments(self, parser):
-
         parser.add_argument('netid')
 
     def handle(self, *args, **options):
+        netid = options['netid']
 
-        for netid in args:
-
-            try:
-                user = UserModel.objects.get(netid=netid)
-            except UserModel.DoesNotExist:
-                err = ("User with netid '%s' does not exist in the database "
-                       "Have them log in first, then retry this command")
-                self.stderr.write(err % netid)
-            else:
-                user.is_superuser = False
-                user.save()
-                msg = "Superuser status revoked for User '%s'"
-                self.stdout.write(msg % user)
-
+        try:
+            user = UserModel.objects.get(netid=netid)
+        except UserModel.DoesNotExist:
+            err = ("User with netid '%s' does not exist in the database "
+                   "Have them log in first, then retry this command")
+            self.stderr.write(err % netid)
+        else:
+            user.is_superuser = False
+            user.save()
+            msg = "Superuser status revoked for User '%s'"
+            self.stdout.write(msg % user)
--- fyt/users/management/commands/setsuperuser.py
+++ fyt/users/management/commands/setsuperuser.py
@@ -4,32 +4,25 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 UserModel = get_user_model()
 
+
 class Command(BaseCommand):
 
     help = 'Give the specified user superuser status'
 
     def add_arguments(self, parser):
-
         parser.add_argument('netid')
 
     def handle(self, *args, **options):
+        netid = options['netid']
 
-        for netid in args:
-
-            try:
-                user = UserModel.objects.get(netid=netid)
-            except UserModel.DoesNotExist:
-                err = ("User with netid '%s' does not exist in the database "
-                       "Have them log in first, then retry this command")
-                self.stderr.write(err % netid)
-            else:
-                user.is_superuser = True
-                user.save()
-                msg = "User '%s' promoted to superuser"
-                self.stdout.write(msg % user)
-
-
-
-
-
-
+        try:
+            user = UserModel.objects.get(netid=netid)
+        except UserModel.DoesNotExist:
+            err = ("User with netid '%s' does not exist in the database "
+                   "Have them log in first, then retry this command")
+            self.stderr.write(err % netid)
+        else:
+            user.is_superuser = True
+            user.save()
+            msg = "User '%s' promoted to superuser"
+            self.stdout.write(msg % user)
