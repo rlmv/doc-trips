5c8c30d7da1b5d49154d59ed0eb9efca75d70d34
Bo Marchman <bo.marchman@gmail.com>
2016-06-03 13:31:30 +0200

    Refactor _make_link usage

--- fyt/db/templatetags/links.py
+++ fyt/db/templatetags/links.py
@@ -8,13 +8,6 @@ from fyt.db.urlhelpers import reverse_create_url
 
 register = template.Library()
 
-def _make_link(url, text):
-    """ Format a link. """
-    html = '<a href="{}">{}</a>'.format(url, text)
-    return mark_safe(html)
-
-make_link = _make_link
-
 
 def pass_null(func):
     """
@@ -32,22 +25,38 @@ def pass_null(func):
     return wraps(func)(wrapper)
 
 
+def _make_link(url, text, default_text=None):
+    """ Format a link.
+
+    Arguments:
+        url (str): The url to put in the link
+        text (str): Text passed to the filter to use as the value of the link
+        default_text (str): Default value to use if text is None
+    """
+    if text is None and default_text is None:
+        raise ValueError('Must provide either `text` or `default_text`')
+
+    if text is None:
+        text = default_text
+
+    html = '<a href="{}">{}</a>'.format(url, text)
+    return mark_safe(html)
+
+make_link = _make_link
+
+
 @register.filter
 @pass_null
 def edit_link(db_object, text=None):
     """ Insert html link to edit db_object. """
-    if text is None:
-        text = 'edit'
-    return _make_link(db_object.update_url(), text)
+    return _make_link(db_object.update_url(), text, 'edit')
 
 
 @register.filter
 @pass_null
 def delete_link(db_object, text=None):
     """ Insert html link to delete db_object. """
-    if text is None:
-        text = 'delete'
-    return _make_link(db_object.delete_url(), text)
+    return _make_link(db_object.delete_url(), text, 'delete')
 
 
 @register.simple_tag
@@ -65,12 +74,7 @@ def detail_link(db_obj, text=None):
     """
     Html link to detailed view for object.
     """
-    def to_str(obj):
-        if text is None:
-            return str(obj)
-        return text
-
-    to_link = lambda obj: _make_link(obj.detail_url(), to_str(obj))
+    to_link = lambda obj: _make_link(obj.detail_url(), text, str(obj))
     return mark_safe(', '.join(map(to_link, as_list(db_obj))))
 
 
