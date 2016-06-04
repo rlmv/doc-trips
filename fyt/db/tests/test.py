b03f7c418fc21171fc20c8f28e21e511f66d219a
Bo Marchman <bo.marchman@gmail.com>
2016-06-04 08:01:18 -0700

    Tidy trips year tests

--- fyt/db/tests/test.py
+++ fyt/db/tests/test.py
@@ -1,24 +1,11 @@
-import unittest
-
-from django.db import models
-from django.forms.models import model_to_dict
-from django.contrib.auth import get_user_model
-from django.test.utils import override_settings
-from django.conf.urls import patterns, url, include
-from django.core.urlresolvers import reverse
-from django.core.exceptions import ValidationError, FieldError
 from model_mommy import mommy
 
 from fyt.test.testcases import WebTestCase, TripsYearTestCase
-from fyt.db.models import DatabaseModel, TripsYear
 from fyt.trips.models import Campsite, TripTemplate
 from fyt.db.forms import tripsyear_modelform_factory
 
 
 class DatabaseModelTestCase(TripsYearTestCase):
-
-    def setUp(self):
-        self.init_current_trips_year()
 
     def test_trips_year_field_is_required(self):
         self.assertRaises(ValueError, mommy.make, Campsite, trips_year=None)
@@ -33,32 +20,20 @@ class DatabaseModelTestCase(TripsYearTestCase):
 
 
 class DatabaseMixinTestCase(WebTestCase):
-
     """ DatabaseMixin integration tests """
-
     csrf_checks = False
 
-    def setUp(self):
-        self.init_current_trips_year()
-        self.init_old_trips_year()
-
     def test_need_database_permissions_to_access_database_pages(self):
+        trips_year = self.init_current_trips_year()
+        campsite = mommy.make(Campsite, trips_year=trips_year)
 
-        campsite = mommy.make(Campsite, trips_year=self.trips_year)
         url = campsite.update_url()
-
-        self.mock_user()
-        self.app.get(url, user=self.user.netid, expect_errors=True)
-
-        self.mock_director()
-        response = self.app.get(url, user=self.director.netid)
-        self.assertEquals(response.status_code, 200)
+        self.app.get(url, user=self.mock_user(), status=403)
+        self.app.get(url, user=self.mock_director(), status=200)
 
 
 class FormFieldCallbackTestCase(TripsYearTestCase):
 
-    def setUp(self):
-        self.init_current_trips_year()
-
     def test_formfield_callback_for_non_DatabaseModel_fields_does_not_raise_error(self):
-        tripsyear_modelform_factory(Campsite, self.current_trips_year, fields='__all__')
+        trips_year = self.init_trips_year()
+        tripsyear_modelform_factory(Campsite, trips_year, fields='__all__')
