931042e691eb8a3583ba0c08aff8782e2d393b3a
Bo Marchman <bo.marchman@gmail.com>
2016-06-27 11:58:45 -0700

    Add filter for external bus requests

--- fyt/incoming/filters.py
+++ fyt/incoming/filters.py
@@ -1,16 +1,19 @@
 
 import django_filters
 from django import forms
+from django.db.models import Q
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit, Layout, Row, Div
 
 from fyt.incoming.models import Registration
+from fyt.transport.models import Stop
 
 EXCHANGE = 'is_exchange'
 TRANSFER = 'is_transfer'
 INTERNATIONAL = 'is_international'
 NATIVE = 'is_native'
 FYSEP = 'is_fysep'
+BUS = 'bus'
 
 
 def BooleanFilter(field, name):
@@ -29,6 +32,24 @@ def BooleanFilter(field, name):
     return filter
 
 
+class ExternalBusRequestFilter(django_filters.ModelChoiceFilter):
+    """Filter registrations based on the bus they requested."""
+
+    def __init__(self, trips_year, **kwargs):
+        qs = Stop.objects.external(trips_year)
+        super().__init__(queryset=qs, **kwargs)
+
+    def filter(self, qs, value):
+        if not value:
+            return qs
+
+        return qs.filter(
+            Q(bus_stop_round_trip=value) |
+            Q(bus_stop_to_hanover=value) |
+            Q(bus_stop_from_hanover=value)
+        )
+
+
 class RegistrationFilterSet(django_filters.FilterSet):
 
     class Meta:
@@ -43,6 +64,9 @@ class RegistrationFilterSet(django_filters.FilterSet):
         self.filters[INTERNATIONAL] = BooleanFilter(INTERNATIONAL, 'International')
         self.filters[NATIVE] = BooleanFilter(NATIVE, 'Native')
         self.filters[FYSEP] = BooleanFilter(FYSEP, 'FYSEP')
+
+        self.filters[BUS] = ExternalBusRequestFilter(
+            trips_year, label='External Bus Request')
 
         self.form.helper = FilterSetFormHelper(self.form)
 
@@ -62,5 +86,6 @@ class FilterSetFormHelper(FormHelper):
             filter_row(INTERNATIONAL),
             filter_row(NATIVE),
             filter_row(FYSEP),
+            filter_row(BUS),
             filter_row(Submit('submit', 'Filter', css_class='btn-block'))
         )
