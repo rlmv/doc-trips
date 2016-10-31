236dc90b5a9c9bf34df11ee5c38519bb93d14678
Bo Marchman <bo.marchman@gmail.com>
2016-10-31 12:46:10 -0400

    Remove deprecated url `patterns`

--- fyt/croos/urls.py
+++ fyt/croos/urls.py
@@ -1,15 +1,14 @@
 
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from fyt.db.urlhelpers import DB_REGEX
 from fyt.croos.views import (CrooListView, CrooCreateView, CrooUpdateView,
                              CrooDetailView, CrooDeleteView)
 
-croo_urlpatterns = patterns(
-    '',
+croo_urlpatterns = [
     url(DB_REGEX['LIST'], CrooListView.as_view(), name='index'),
     url(DB_REGEX['CREATE'], CrooCreateView.as_view(), name='create'),
     url(DB_REGEX['UPDATE'], CrooUpdateView.as_view(), name='update'),
     url(DB_REGEX['DETAIL'], CrooDetailView.as_view(), name='detail'),
     url(DB_REGEX['DELETE'], CrooDeleteView.as_view(), name='delete')
-)
+]
