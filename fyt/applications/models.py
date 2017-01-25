61d2e8f9646f4a6e9e4d8e65d53c725e5b3d483a
Bo Marchman <bo.marchman@gmail.com>
2017-01-25 15:11:39 -0500

    Remove unused import

--- fyt/applications/models.py
+++ fyt/applications/models.py
@@ -1,7 +1,6 @@
 
 from django.conf import settings
 from django.core.exceptions import ValidationError
-from django.core.urlresolvers import reverse
 from django.db import models
 from django.db.models import Q
 
