2626bfabbd20194fc7202c58a703d28d243d504b
Bo Marchman <bo.marchman@gmail.com>
2017-02-12 16:46:28 -0500

    Remove old section and triptype choice fields

--- fyt/trips/fields.py
+++ fyt/trips/fields.py
@@ -3,25 +3,8 @@ from django import forms
 from django.utils.safestring import mark_safe
 
 
-class LeaderSectionChoiceField(forms.ModelMultipleChoiceField):
-    """
-    Section choice field with TRIPPEE dates.
-    """
-    def label_from_instance(self, obj):
-        return mark_safe(obj.name + ' &mdash; '  + obj.leader_date_str())
-
-
-class TrippeeSectionChoiceField(forms.ModelMultipleChoiceField):
-    """ 
-    Section choice field with TRIPPEE dates.
-    """
-    def label_from_instance(self, obj):
-        label = '%s &mdash; %s' % (obj.name, obj.trippee_date_str())
-        return mark_safe(label)
-
-
 class TripChoiceField(forms.ModelChoiceField):
-    """ 
+    """
     Field with verbose Trip labels .
     """
     def label_from_instance(self, obj):
@@ -30,5 +13,3 @@ class TripChoiceField(forms.ModelChoiceField):
             obj.template.triptype.name,
             obj.template.description_summary
         )
-
-
