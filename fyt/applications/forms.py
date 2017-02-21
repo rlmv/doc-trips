0c874256cc09ec3bcbb4578e962831eb2b5ca330
Bo Marchman <bo.marchman@gmail.com>
2017-02-21 12:26:31 -0500

    Tweak wording

--- fyt/applications/forms.py
+++ fyt/applications/forms.py
@@ -557,8 +557,8 @@ class LeaderSupplementLayout(Layout):
                 HTML(
                     "<p>As we mentioned before, outdoor ability is NOT "
                     "required to volunteer for Trips, but certain croos and "
-                    "trips do require wilderness skills, so these questions "
-                    "will help us place you appropriately.</p>"
+                    "trips do require wilderness skills. These questions will"
+                    "help us place you appropriately.</p>"
                 ),
                 'class_2_3_paddler',
                 'ledyard_level_1',
