fc88cf3a796c67ce9734f65f307520c4a37d771b
Bo Marchman <bo.marchman@gmail.com>
2017-04-11 17:11:40 -0400

    Add Training and Session model

--- fyt/training/models.py
+++ fyt/training/models.py
@@ -1,3 +1,35 @@
+
 from django.db import models
 
-# Create your models here.
+from fyt.db.models import DatabaseModel
+
+
+class Training(DatabaseModel):
+    """
+    A type of training.
+    """
+    class Meta:
+        ordering = ['name']
+
+    name = models.CharField(max_length=64)
+    description = models.TextField(blank=True)
+
+
+class Session(DatabaseModel):
+    """
+    A scheduled session for a certain training type.
+    """
+    class Meta:
+        ordering = ['time']
+
+    training = models.ForeignKey(Training)
+    time = models.DateTimeField()
+    duration = models.DurationField()
+
+#
+# class Signup(DatabaseModel):
+#     """
+#     A user's registration for a particular training session.
+#     """
+#     volunteer = models.ForeignKey(Volunteer)
+#     session = models.ForeignKey(Session)
