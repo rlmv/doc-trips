da38039572a0d5916fb254358b7b6df15e7b2506
Bo Marchman <bo.marchman@gmail.com>
2017-08-01 06:35:35 -0400

    Add todos

--- fyt/transport/signals.py
+++ fyt/transport/signals.py
@@ -4,6 +4,8 @@ from django.dispatch import receiver
 from fyt.transport.models import InternalBus, StopOrder, Stop
 from fyt.trips.models import Trip, TripTemplate
 
+# TODO: update ordering for Section.date changes
+# Ensure Trip.section and Trip.template cannot change
 
 def create_dropoff(bus, trip):
     return StopOrder.objects.create(
