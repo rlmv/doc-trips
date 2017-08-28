24d7cb0ab9618176e36ecdbdcc5ad20ac2dcb3d3
Bo Marchman <bo.marchman@gmail.com>
2017-08-28 11:30:24 -0400

    Add missing test file

---
+++ fyt/permissions/tests.py
@@ -0,0 +1,35 @@
+from django.core.urlresolvers import reverse
+
+from fyt.test import FytTestCase
+from fyt.permissions.permissions import Group, GroupRegistry, can_view_database
+
+
+class GroupRegistryTestCase(FytTestCase):
+
+    def test_group_registry(self):
+        groups = GroupRegistry({
+            'bus drivers': [can_view_database]
+        })
+        self.assertEqual(groups.bus_drivers.name, 'bus drivers')
+        self.assertQsEqual(groups.bus_drivers.permissions.all(),
+                           [can_view_database()])
+
+    def test_group_registry_attribute_error(self):
+        groups = GroupRegistry({})
+        with self.assertRaises(AttributeError):
+            groups.olcs
+
+    def test_group_registry_bootstrap(self):
+        groups = GroupRegistry({
+            'drivers': [can_view_database]
+        })
+        self.assertFalse(Group.objects.filter(name='drivers').exists())
+        groups.bootstrap()
+        self.assertTrue(Group.objects.filter(name='drivers').exists())
+
+
+class SetPermissionsViewTestCase(FytTestCase):
+
+    def test_get(self):
+        url = reverse('permissions:set_permissions')
+        self.app.get(url, user=self.make_director())
