46d4f821cfba060e11708e1f662e6f011116407f
Bo Marchman <bo.marchman@gmail.com>
2018-02-13 13:10:33 -0500

    Add robots.txt

--- fyt/urls.py
+++ fyt/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 from fyt.incoming.urls import settings_urlpatterns
 from fyt.permissions.permissions import groups
@@ -25,6 +26,8 @@ urlpatterns = [
     url(r'^training/', include(('fyt.training.urls', 'training'))),
     url(r'^users/', include(('fyt.users.urls', 'users'))),
     url(r'^volunteers/', include(('fyt.applications.urls', 'applications'))),
+    url(r'^robots\.txt$', TemplateView.as_view(
+        template_name='robots.txt', content_type='text/plain'))
 ]
 
 # Install django-debug-toolbar
