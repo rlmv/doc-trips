715bb45c89f6e3c278b45264527338f2717334ae
Bo Marchman <bo.marchman@gmail.com>
2018-04-09 15:57:08 -0400

    Delete legacy grade models

--- fyt/applications/admin.py
+++ fyt/applications/admin.py
@@ -1,24 +1,11 @@
-
 from django.contrib import admin
 
 from fyt.applications.models import (
-    CrooApplicationGrade,
     CrooSupplement,
-    LeaderApplicationGrade,
     LeaderSupplement,
-    QualificationTag,
-    SkippedCrooGrade,
-    SkippedLeaderGrade,
-    Volunteer,
+    Volunteer
 )
-
 
 admin.site.register(Volunteer)
 admin.site.register(CrooSupplement)
 admin.site.register(LeaderSupplement)
-admin.site.register(CrooApplicationGrade)
-admin.site.register(LeaderApplicationGrade)
-admin.site.register(QualificationTag)
-admin.site.register(SkippedCrooGrade)
-admin.site.register(SkippedLeaderGrade)
-
