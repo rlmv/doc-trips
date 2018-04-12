0c5d3580f82df3fde112f61d3074464ba782bcf9
Bo Marchman <bo.marchman@gmail.com>
2018-04-12 14:26:00 -0400

    Combine test files

--- fyt/core/tests/test_trips_year.py
+++ fyt/core/tests/test_trips_year.py
@@ -92,3 +92,14 @@ class TripsYearMixinTestCase(FytTestCase):
         view = TripsYearMixin()
         view.kwargs = {'trips_year': self.trips_year.year}
         self.assertEqual(view.trips_year, self.trips_year)
+
+
+class DetailViewTestCase(FytTestCase):
+
+    def test_urls_in_context(self):
+        trips_year = self.init_trips_year()
+        # test Section detail, for example
+        section = mommy.make(Section, trips_year=trips_year)
+        resp = self.app.get(section.detail_url(), user=self.make_director())
+        self.assertEqual(resp.context['update_url'], section.update_url())
+        self.assertEqual(resp.context['delete_url'], section.delete_url())
