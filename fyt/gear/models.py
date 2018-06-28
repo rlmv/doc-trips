96a75b75f265c80caefea5c781140024a423b443
Bo Marchman <bo.marchman@gmail.com>
2018-06-28 16:07:31 -0700

    Validate gear requester

--- fyt/gear/models.py
+++ fyt/gear/models.py
@@ -1,6 +1,9 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
+from fyt.applications.models import Volunteer
 from fyt.core.models import DatabaseModel
+from fyt.incoming.models import IncomingStudent
 from fyt.users.models import DartmouthUser
 
 
@@ -12,12 +15,31 @@ class Gear(DatabaseModel):
 
 
 class GearRequest(DatabaseModel):
-    user = models.ForeignKey(DartmouthUser, editable=False,
-                             on_delete=models.PROTECT)
     gear = models.ManyToManyField(Gear)
     additional = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    incoming_student = models.OneToOneField(
+        IncomingStudent,
+        editable=False,
+        null=True,
+        on_delete=models.PROTECT)
+
+    volunteer = models.OneToOneField(
+        Volunteer,
+        editable=False,
+        null=True,
+        on_delete=models.PROTECT)
+
+    @property
+    def requester(self):
+        return self.incoming_student or self.volunteer
+
+    def clean(self):
+        if not (bool(self.incoming_student) ^ bool(self.volunteer)):
+            raise ValidationError('A gear request must either be for an '
+                                  'incoming student or a trips volunteer')
 
     def __str__(self):
         return 'GearRequest ({})'.format(self.user)
--- fyt/gear/tests.py
+++ fyt/gear/tests.py
@@ -1,4 +1,37 @@
-from django.test import TestCase
+from django.core.exceptions import ValidationError
+from model_mommy import mommy
+
+from .models import GearRequest
+
+from fyt.test import FytTestCase
 
 
-# Create your tests here.
+class GearRequestModelTestCase(FytTestCase):
+
+    def setUp(self):
+        self.init_trips_year()
+
+    def test_validate_volunteer_or_incoming(self):
+        # Neither incoming or volunteer
+        with self.assertRaises(ValidationError):
+            mommy.make(GearRequest).full_clean()
+
+        # Can't be both incoming and volunteer
+        with self.assertRaises(ValidationError):
+            mommy.make(
+                GearRequest,
+                incoming_student__trips_year=self.trips_year,
+                volunteer__trips_year=self.trips_year
+            ).full_clean()
+
+    def test_incoming_student_requester(self):
+        gear_request = mommy.make(
+            GearRequest,
+            incoming_student__trips_year=self.trips_year)
+        self.assertEqual(gear_request.incoming_student, gear_request.requester)
+
+    def test_volunteer_requester(self):
+        gear_request = mommy.make(
+            GearRequest,
+            volunteer__trips_year=self.trips_year)
+        self.assertEqual(gear_request.volunteer, gear_request.requester)
