0ca85e3850780e7eb2575bc32738d2ebd5a6f922
Bo Marchman <bo.marchman@gmail.com>
2018-08-13 14:14:34 -0400

    Show gear request in public portals

--- fyt/gear/templatetags/gear.py
+++ fyt/gear/templatetags/gear.py
@@ -9,3 +9,11 @@ def gear_request(req):
     return {
         'gear_request': req
     }
+
+
+@register.inclusion_tag('gear/_request_public.html')
+def gear_request_portal(req):
+    """For leader and trippee portal."""
+    return {
+        'gear_request': req
+    }
