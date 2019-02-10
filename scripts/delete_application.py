7387fc4c3bf4edf67967bba4012733ee942c1182
Bo Marchman <bo.marchman@gmail.com>
2019-02-10 09:51:05 -0500

    Fix delete application script

--- scripts/delete_application.py
+++ scripts/delete_application.py
@@ -1,7 +1,9 @@
 from fyt.applications.models import Volunteer
-v = Volunteer.objects.get(applicant__netid='d34898x')
-v.leader_supplement.delete()
-v.croo_supplement.delete()
-v.attendee.delete()
-v.first_aid_certifications.all().delete()
-v.delete()
+
+def run():
+    v = Volunteer.objects.get(applicant__netid='d34898x', trips_year__is_current=True)
+    v.leader_supplement.delete()
+    v.croo_supplement.delete()
+    v.attendee.delete()
+    v.first_aid_certifications.all().delete()
+    v.delete()
