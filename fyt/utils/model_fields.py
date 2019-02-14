74163f404001de01dc9ad5e6ccd0504215048683
Bo Marchman <bo.marchman@gmail.com>
2019-02-14 17:56:17 -0500

    Add None as choice to NullYesNo

--- fyt/utils/model_fields.py
+++ fyt/utils/model_fields.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 YES_NO_CHOICES = ((True, 'Yes'), (False, 'No'))
+YES_NO_NONE_CHOICES = ((None, '----'), (True, 'Yes'), (False, 'No'))
 
 
 def YesNoField(*args, **kwargs):
@@ -11,7 +12,8 @@ def YesNoField(*args, **kwargs):
 
 
 def NullYesNoField(*args, **kwargs):
-    kwargs['choices'] = YES_NO_CHOICES
+    kwargs['choices'] = YES_NO_NONE_CHOICES
     kwargs['default'] = None
     kwargs['null'] = True
+    kwargs['blank'] = True
     return models.BooleanField(*args, **kwargs)
