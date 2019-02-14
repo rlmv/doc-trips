5e8448155e0a0ee695a556bc2144ad63b5137051
Bo Marchman <bo.marchman@gmail.com>
2019-02-14 17:47:48 -0500

    Remove NullBooleanField usage

--- fyt/utils/model_fields.py
+++ fyt/utils/model_fields.py
@@ -13,4 +13,5 @@ def YesNoField(*args, **kwargs):
 def NullYesNoField(*args, **kwargs):
     kwargs['choices'] = YES_NO_CHOICES
     kwargs['default'] = None
-    return models.NullBooleanField(*args, **kwargs)
+    kwargs['null'] = True
+    return models.BooleanField(*args, **kwargs)
